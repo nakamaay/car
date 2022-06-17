4bd7e49ca2cee25f66de31e4fb6b459ee63c182e
nakamaay <pcyama7ka@gmail.com>
2022-06-17 15:27:32 +0900

    ウインドウ表示

--- main.cpp
+++ main.cpp
@@ -1,9 +1,35 @@
+#include <cstdlib>
 #include <iostream>
-
-using namespace std;
+#include <GLFW/glfw3.h>
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    if (glfwInit() == GL_FALSE)
+    {
+        std::cerr << "Can't initialize GLFW" << std::endl;
+        return 1;
+    }
+    atexit(glfwTerminate);
+
+    GLFWwindow *const window(glfwCreateWindow(640, 480, "Hello!", NULL, NULL));
+    if (window == NULL)
+    {
+        std::cerr << "Can't create GLFW window." << std::endl;
+        return 1;
+    }
+
+    glfwMakeContextCurrent(window);
+
+    glClearColor(0.2f, 0.2f, 0.2f, 0.0f);
+
+    // loop
+
+    while (glfwWindowShouldClose(window) == GL_FALSE)
+    {
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        glfwSwapBuffers(window);
+        glfwWaitEvents();
+    }
     return 0;
 }
